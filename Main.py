624a71cbf3c2908b5ef3b21fa794bc3cd2fe7a5d
amir78729 <amir78729@gmail.com>
2021-05-06 17:43:45 +0430

    seprate database for passwords and data

--- Main.py
+++ Main.py
@@ -10,17 +10,39 @@ def sort_tuple(tup):
 def get_list_without_redundancy(lst):
     return list(dict.fromkeys(lst))
 
+
+def normalize(word):
+    suffixes = ['ات', 'ها', 'ی']
+    exceptions = ['انتها', 'زحمات', 'ملی']
+    for s in suffixes:
+        if word.endswith(s):
+            if word not in exceptions:
+                print(word)
+                word = word.replace(s, '')
+                print(word)
+    return word
+
+def calculate_frequency(all_tokens_, term_doc_id_):
+    count = {}
+    for i in all_tokens_:
+        # print(i)
+        freq = 0
+        for j in term_doc_id_:
+            if j[0] == i:
+                freq += 1
+        # print(freq)
+        count.update({i: freq})
+    return count
+
 def create_inverted_index(term_doc_id):
     res = {}
     for x in term_doc_id:
         if not x[0] in res.keys():
             list = []
-            list.append(x[1])
-            res.update({x[0]: list})
         else:
             list = res[x[0]]
-            list.append(x[1])
-            res.update({x[0]: list})
+        list.append(x[1])
+        res.update({x[0]: list})
 
     # print(type(res))
     for r in res.keys():
@@ -47,6 +69,7 @@ for i in range(2):
         term = term.replace('.', '')
         term = term.replace(':', '')
         term = term.replace('،', '')
+        term = normalize(term)
         term_doc_id.append((term, i + 1))
 
 term_doc_id = sort_tuple(term_doc_id)
@@ -56,16 +79,9 @@ create_inverted_index(term_doc_id)
 all_tokens = list(dict.fromkeys([t[0] for t in get_list_without_redundancy(term_doc_id)]))
 # print(all_tokens)
 
-count = {}
-for i in all_tokens:
-    # print(i)
-    freq = 0
-    for j in term_doc_id:
-        if j[0] == i:
-            freq += 1
-    # print(freq)
-    count.update({i: freq})
 
+frequency = calculate_frequency(all_tokens, term_doc_id)
+print(frequency)
 
 # print(count["و"])
 # for size in 1, 2:
