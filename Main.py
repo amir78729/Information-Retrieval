09131a63ec5764e6d84148f7ce98f2ff9f370619
amir78729 <amir78729@gmail.com>
2021-05-24 01:18:23 +0430

    remove prefix function

--- Main.py
+++ Main.py
@@ -4,23 +4,33 @@ from nltk import ngrams, FreqDist
 
 # Function to sort the list by second item of tuple
 def sort_tuple(tup):
-    return (sorted(tup, key=lambda x: x[0]))
+    return sorted(tup, key=lambda x: x[0])
 
 
+def remove_suffix(s, suffix):
+    if suffix and s.endswith(suffix):
+        return s[:-len(suffix)]
+    return s
+
+# [a, a, b, c, d, d] => [a, b, c, d]
 def get_list_without_redundancy(lst):
     return list(dict.fromkeys(lst))
 
 
 def normalize(word):
     suffixes = ['ات', 'ها', 'ی']
-    exceptions = ['انتها', 'زحمات', 'ملی']
+    exceptions = ['انتها', 'زحمات', 'ملی', 'علی', 'رضی',
+                  'حواشی', 'جنابعالی', 'تازگی', 'آسیایی',
+                  'امریکایی', 'آمریکایی', 'نکویی', 'محمدلویی',
+                  'محسنی', 'وی', 'طی', 'بازی', '', ]
     for s in suffixes:
         if word.endswith(s):
             if word not in exceptions:
                 print(word)
-                word = word.replace(s, '')
+                word = remove_suffix(word, s)
                 print(word)
     return word
+
 
 def calculate_frequency(all_tokens_, term_doc_id_):
     count = {}
@@ -33,6 +43,7 @@ def calculate_frequency(all_tokens_, term_doc_id_):
         # print(freq)
         count.update({i: freq})
     return count
+
 
 def create_inverted_index(term_doc_id):
     res = {}
@@ -62,14 +73,21 @@ tokens = []
 # print(content[0].split())
 
 all_counts = dict()
+
 term_doc_id = []
+
 for i in range(2):
+    # make a list of words retrieved from content
     doc_terms = content[i].split()
+
+    # process tokens
     for term in doc_terms:
         term = term.replace('.', '')
         term = term.replace(':', '')
         term = term.replace('،', '')
         term = normalize(term)
+
+        # add {TERM: DocID} to our dictionary
         term_doc_id.append((term, i + 1))
 
 term_doc_id = sort_tuple(term_doc_id)
@@ -81,6 +99,7 @@ all_tokens = list(dict.fromkeys([t[0] for t in get_list_without_redundancy(term_
 
 
 frequency = calculate_frequency(all_tokens, term_doc_id)
+
 print(frequency)
 
 # print(count["و"])
