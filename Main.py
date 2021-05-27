4b2bc422ec2dda016d0314015f85d0223582af0d
amir78729 <amir78729@gmail.com>
2021-05-27 21:25:13 +0430

    query processing

--- Main.py
+++ Main.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from tqdm import tqdm
+import time
+
+
+def strikedthrough(text):
+    result = ''
+    for c in text:
+        result = result + c + '\u0336'
+    return result
 
 
 class SearchEngine:
@@ -87,14 +95,21 @@ class SearchEngine:
                     dictionary.update({bon_maazi + s: bon_maazi})
                     dictionary.update({'ن' + bon_maazi + s: bon_maazi})
                     dictionary.update({'می‌' + bon_maazi + s: bon_maazi})
+                    dictionary.update({'می' + bon_maazi + s: bon_maazi})
                     dictionary.update({'نمی‌' + bon_maazi + s: bon_maazi})
+                    dictionary.update({'نمی' + bon_maazi + s: bon_maazi})
                     dictionary.update({bon_maazi + 'ه‌ا' + s: bon_maazi})
                 for s in mozare_suffixes:
                     dictionary.update({bon_mozare + s: bon_mozare})
                     dictionary.update({'ب' + bon_mozare + s: bon_mozare})
                     dictionary.update({'ن' + bon_mozare + s: bon_mozare})
                     dictionary.update({'نمی‌' + bon_mozare + s: bon_mozare})
+                    dictionary.update({'نمی' + bon_mozare + s: bon_mozare})
                     dictionary.update({'می‌' + bon_mozare + s: bon_mozare})
+                    dictionary.update({'می' + bon_mozare + s: bon_mozare})
+
+                # dictionary.update({'ب' + bon_mozare: bon_mozare})
+                # dictionary.update({'ن' + bon_mozare: bon_mozare})
             return dictionary
 
     def get_mokassar_plurals_dictionary(self):
@@ -187,6 +202,8 @@ class SearchEngine:
         return term
 
     def main(self):
+
+        time.sleep(.5)
         print('NUMBER OF TOKENS: {} ({} DISTINCT VALUES WITHOUT STOPWORDS)'
               .format(len(self.term_doc_id), len(self.inverted_index)))
         # for i in self.all_tokens_frequencies.keys():
@@ -194,39 +211,75 @@ class SearchEngine:
         # for i in self.inverted_index.keys():
         #     print(i, self.inverted_index[i])
         while True:
-            query = self.normalize(input('enter a query (\"\\q\": quit the program)'.upper()))
-            if query == '\\Q':
+            user_queries = input('enter a query (\"\\q\": quit the program)'.upper()).split()
+
+            if user_queries[0] == '\\Q' and len(user_queries) == 1:
                 break
             else:
-                try:
-                    if query in self.stopwords:
-                        print('this word is a stopword'.upper())
+                set_of_answers = []
+                missing_words = []
+                for query in user_queries:
+                    try:
+                        if query in self.stopwords:
+                            print(query, 'is a stopword'.upper())
+                        else:
+                            result = sorted(list(set(self.inverted_index[self.normalize(query)])))
+                            print('{} result(s) founded for {}.'.format(len(result), query).upper())
+                            set_of_answers.append(set(result))
+
+                    except KeyError:
+                        print('no result for'.upper(), query)
+                        missing_words.append(query)
+
+            # print(set_of_answers)
+            print('combining results...'.upper())
+            try:
+                result = list(set.intersection(*set_of_answers))
+            except TypeError:
+                print('NO ANSWERS')
+                print(45 * '- ')
+                continue
+
+            if len(result) == 0:
+                print('NO ANSWERS')
+                print(45 * '- ')
+                continue
+
+            # print(result)
+            for r in range(len(result)):
+                if r != len(result) - 1:
+                    print(' ├─ {}'.format(result[r]))
+                else:
+                    print(' └─ {}'.format(result[r]))
+
+            if len(missing_words) != 0:
+                print('( missing words: '.upper(), end='')
+                for i in range(len(missing_words)):
+                    if i == len(missing_words)-1:
+                        print(missing_words[i], ')')
                     else:
-                        result = list(set(self.inverted_index[query]))
-                        print('{} result(s):'.format(len(result)).upper())
-                        print(result)
-                        while True:
-                            res = input('select on of the results to show the information (-1 to cancel)'.upper())
-                            for fa, en in zip(self.FA_NUMS, self.EN_NUMS):
-                                res = res.replace(fa, en)
-                            if res == '-1':
-                                print('canceled'.upper())
-                                break
-                            else:
-                                try:
-                                    if int(res) in result:
-                                        print('DOC-ID : ' + str(self.doc_id[int(res) - 1]))
-                                        print('LINK   : ' + self.url[int(res) - 1])
-                                        print('CONTENT: \n' + self.content[int(res) - 1])
+                        print(strikedthrough(missing_words[i]), end=', ')
 
-                                    else:
-                                        print('bad input!'.upper())
-                                except Exception as e:
-                                    print(e)
-                                    print('bad input!'.upper())
+            while True:
+                res = input('select on of the results to show the information (-1 to cancel)'.upper())
+                for fa, en in zip(self.FA_NUMS, self.EN_NUMS):
+                    res = res.replace(fa, en)
+                if res == '-1':
+                    print('canceled'.upper())
+                    break
+                else:
+                    try:
+                        if int(res) in result:
+                            print('DOC-ID : ' + str(self.doc_id[int(res) - 1]))
+                            print('LINK   : ' + self.url[int(res) - 1])
+                            print('CONTENT: \n' + self.content[int(res) - 1])
 
-                except KeyError:
-                    print('no result'.upper())
+                        else:
+                            print('bad input!'.upper())
+                    except Exception as e:
+                        print(e)
+                        print('bad input!'.upper())
+
             print(45 * '- ')
 
 
