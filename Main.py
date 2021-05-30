fa55b0076a40236a5ca686e7e6a1e22aa14fde55
amir78729 <amir78729@gmail.com>
2021-05-31 00:12:44 +0430

    substing using ""

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tqdm import tqdm
 import time
+import re
 
 
 def strikedthrough(text):
@@ -12,6 +13,7 @@ def strikedthrough(text):
 
 class SearchEngine:
     def __init__(self):
+        start = time.time()
         path = 'IR_Spring2021_ph12_7k.xlsx'
         # path = 'demo data.xlsx'
 
@@ -24,7 +26,7 @@ class SearchEngine:
         self.FA_NUMS = '۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹ ٠ ۰'.split()
         self.EN_NUMS = '1 2 3 4 5 6 7 8 9 0 0'.split()
         self.SYMBOLS_TO_BE_REMOVED = '_ + = & ؟ ^ % $ . : ، \" \' | \\ / * ) ( ! - ؛'.split()
-        self.STOPWORDS_LIMIT = 10
+        self.STOPWORDS_LIMIT = 20
 
         # dictionaries
         self.stemming_conversion_dictionary = self.get_stemming_dictionary()
@@ -46,13 +48,14 @@ class SearchEngine:
             doc_terms = self.content[i].split()
 
             # process tokens
+            position_index = 0
             for term in doc_terms:
 
                 term = self.normalize(term)
 
-                # add {TERM: DocID} to our dictionary
-                self.term_doc_id.append((term, i + 1))
-
+                # add {TERM: (DocID, PositionalIndex}) to our dictionary
+                self.term_doc_id.append((term, (i+1, position_index)))
+                position_index += 1
                 # creating a list from distinct tokens
                 if term not in self.all_tokens_frequencies.keys():
                     self.all_tokens_frequencies.update({term: 1})
@@ -70,18 +73,29 @@ class SearchEngine:
         # removing stopwords
         [self.stopwords.append(list(self.all_tokens_frequencies.keys())[-1 - sw])
          for sw in tqdm(range(self.STOPWORDS_LIMIT), desc='FINDING ALL OF STOPWORDS')]
-        print('STOPWORDS: {  ', end='')
-        [print(sw, end='\t') for sw in self.stopwords]
-        print('}')
+        # time.sleep(.1)
+        # print('STOPWORDS: {  ', end='')
+        # [print(sw, end='\t') for sw in self.stopwords]
+        # print('}')
         [self.inverted_index.pop(sw) for sw in tqdm(self.stopwords, desc='REMOVING SOME STOPWORDS ')]
-
-
-
-
+        end = time.time()
+        self.process_time = end - start
 
     # Function to sort the list by second item of tuple
     def sort_tuple(self, tup):
         return sorted(tup, key=lambda x: x[0])
+
+    # 'this is the "table of content" of our text' => ['table of content', 'this', 'is', 'the', 'of', 'our', 'text']
+    def exstract_substring(self, text):
+        match = re.search("\"(.+?)\"", text, flags=re.IGNORECASE)
+        try:
+            substring = match.group(1)
+        except:
+            substring = ""
+        string_without_substring = text.replace(substring, '').replace("\"", "").strip()
+        return substring, string_without_substring.split()
+
+
 
     def get_stemming_dictionary(self):
         with open('stemming_conversion.txt', 'r', encoding='utf-8') as stems:
@@ -140,17 +154,6 @@ class SearchEngine:
                     # print(word)
         return word
 
-    def calculate_frequency(self, all_tokens_, term_doc_id_):
-        count = {}
-        for i in tqdm(all_tokens_, desc='CALCULATING FREQUENCIES '):
-            # print(i)
-            freq = 0
-            for j in term_doc_id_:
-                if j[0] == i:
-                    freq += 1
-            count.update({i: freq})
-        return count
-
     def create_inverted_index(self, term_doc_id):
         res = {}
         for x in tqdm(term_doc_id, desc='CREATING INVERTED INDEX '):
@@ -186,6 +189,8 @@ class SearchEngine:
         for symbol in self.SYMBOLS_TO_BE_REMOVED:
             term = term.replace(symbol, '')
 
+        term = term.replace('آ', 'ا')
+
         # removing suffixes
         term = self.removing_suffixes(term)
 
@@ -203,34 +208,89 @@ class SearchEngine:
 
     def main(self):
 
-        time.sleep(.5)
-        print('NUMBER OF TOKENS: {} ({} DISTINCT VALUES WITHOUT STOPWORDS)'
-              .format(len(self.term_doc_id), len(self.inverted_index)))
+        time.sleep(1)
+        print('SEARCH ENGINE IS READY ({} seconds)'.format(self.process_time))
+        print(' ├─ {} STOPWORDS'.format(len(self.stopwords)))
+        print(' ├─ {} TOKENS '.format(len(self.term_doc_id)))
+        print(' └─ {} DISTINCT VALUES WITHOUT STOPWORDS'.format(len(self.inverted_index)))
         # for i in self.all_tokens_frequencies.keys():
         #     print(i, self.all_tokens_frequencies[i])
         # for i in self.inverted_index.keys():
         #     print(i, self.inverted_index[i])
         while True:
-            user_queries = input('enter a query (\"\\q\": quit the program)'.upper()).split()
-
-            if user_queries[0] == '\\Q' and len(user_queries) == 1:
+            user_queries = input('enter a query \n'
+                                 '(\"\\q\": quit the program)\n'
+                                 '(you can use \"[a substring]\" in search)\n> '.upper())
+            substring, string_without_substring = self.exstract_substring(user_queries)
+            start_time = time.time()
+            if user_queries == '':
+                print("EMPTY QUERY!")
+                continue
+            if user_queries[0].split() == '\\Q' and len(user_queries) == 1:
                 break
             else:
                 set_of_answers = []
                 missing_words = []
-                for query in user_queries:
-                    try:
-                        if query in self.stopwords:
-                            print(query, 'is a stopword'.upper())
-                        else:
-                            result = sorted(list(set(self.inverted_index[self.normalize(query)])))
-                            print('{} result(s) founded for {}.'.format(len(result), query).upper())
-                            set_of_answers.append(set(result))
+                substring_results = set()
 
-                    except KeyError:
-                        print('no result for'.upper(), query)
-                        missing_words.append(query)
+                # processing substring
+                if len(substring) != 0:
+                    print('processing substring...'.upper())
+                    substring = substring.split()
+                    # print(substring)
+                    candidates = []
+                    for s in substring:
+                        candidate = self.inverted_index[self.normalize(s)]
+                        candidates.append(candidate)
 
+                    # only docs
+                    # print(candidates)
+                    docs = []
+                    for c in candidates:
+                        cc = []
+                        for ccc in c:
+                            cc.append(ccc[0])
+                        docs.append(set(cc))
+                    # print(docs)
+
+                    docs = list(set.intersection(*docs))
+                    # print(docs)
+
+                    if len(docs) == 0:
+                        pass
+                    else:
+                        # first element of substring
+                        for base in candidates[0]:
+                            if base[0] in docs:
+                                flag = True
+                                doc = base[0]
+                                index = base[1]
+                                for i in range(len(candidates)-1):
+                                    if (doc, index + i + 1) not in candidates[i+1]:
+                                        flag = False
+                                        break
+                                if flag:
+                                    substring_results.add(doc)
+
+                    set_of_answers.append(substring_results)
+                try:
+                    for query in string_without_substring:
+                        try:
+                            if query in self.stopwords:
+                                print('{} is a stopword'.format(query).upper())
+                            else:
+                                result = sorted(list(set(self.inverted_index[self.normalize(query)])))
+                                print('{} result(s) founded for {}.'.format(len(result), query).upper())
+                                result_docs = []
+                                for i in result:
+                                    result_docs.append(i[0])
+                                set_of_answers.append(set(result_docs))
+
+                        except KeyError:
+                            print('no result for'.upper(), query)
+                            missing_words.append(query)
+                except IndexError:
+                    pass
             # print(set_of_answers)
             print('combining results...'.upper())
             try:
@@ -244,8 +304,8 @@ class SearchEngine:
                 print('NO ANSWERS')
                 print(45 * '- ')
                 continue
-
-            # print(result)
+            end_time = time.time()
+            print(len(result), 'RESULT(S) ({} seconds)'.format((end_time - start_time)))
             for r in range(len(result)):
                 if r != len(result) - 1:
                     print(' ├─ {}'.format(result[r]))
@@ -261,7 +321,7 @@ class SearchEngine:
                         print(strikedthrough(missing_words[i]), end=', ')
 
             while True:
-                res = input('select on of the results to show the information (-1 to cancel)'.upper())
+                res = input('select on of the results to show the information (-1 to cancel)\n> '.upper())
                 for fa, en in zip(self.FA_NUMS, self.EN_NUMS):
                     res = res.replace(fa, en)
                 if res == '-1':
@@ -269,6 +329,9 @@ class SearchEngine:
                     break
                 else:
                     try:
+                        # result_docs = []
+                        # for i in result:
+                        #     result_docs.append(i[0])
                         if int(res) in result:
                             print('DOC-ID : ' + str(self.doc_id[int(res) - 1]))
                             print('LINK   : ' + self.url[int(res) - 1])
