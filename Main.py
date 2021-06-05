166ec2cd4bca8288e1838826076cd19f9d5882c7
amir78729 <amir78729@gmail.com>
2021-06-05 15:30:29 +0430

    final

--- Main.py
+++ Main.py
@@ -4,27 +4,13 @@ import time
 import re
 
 
-def strikedthrough(text):
-    result = ''
-    for c in text:
-        result = result + c + '\u0336'
-    return result
-
-
 class SearchEngine:
     def __init__(self):
-
         start = time.time()
         self.PATH = 'IR_Spring2021_ph12_7k.xlsx'
         # self.PATH = 'demo data.xlsx'
-
         self.MOKASSAAR_PLURALS = 'mokassar_plurals.txt'
-
-        self.data = pd.read_excel(self.PATH)
-        self.doc_id = self.data['id'].tolist()
-        self.content = self.data['content'].tolist()
-        self.url = self.data['url'].tolist()
-
+        self.BON_FILE = 'stemming_conversion.txt'
         # constants
         self.FA_NUMS = '۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹ ٠ ۰'.split()
         self.EN_NUMS = '1 2 3 4 5 6 7 8 9 0 0'.split()
@@ -36,32 +22,28 @@ class SearchEngine:
             'ي': 'ی',
             'ك': 'ک',
         }
-
+        self.data = pd.read_excel(self.PATH)
+        self.doc_id = self.data['id'].tolist()
+        self.content = self.data['content'].tolist()
+        self.url = self.data['url'].tolist()
         # dictionaries
         self.stemming_conversion_dictionary = self.get_stemming_dictionary()
         self.mokassar_plurals_dictionary = self.get_mokassar_plurals_dictionary()
-
         self.all_tokens_frequencies = {}
-
         self.stopwords = []
-
         # reading exception words from file
         with open('normalization_exceptions.txt', 'r', encoding='utf-8') as file:
             self.normalization_exceptions = file.read().split('\n')
-
         # create term-DocID dictionary
         self.term_doc_id = []
         # for i in tqdm(range(len(self.content)), "PROCESSING ALL DOCUMENTS"):
         for i in tqdm(range(100), "PROCESSING ALL DOCUMENTS"):
             # make a list of words retrieved from content
             doc_terms = self.content[i].split()
-
             # process tokens
             position_index = 0
             for term in doc_terms:
-
                 term = self.normalize(term)
-
                 # add {TERM: (DocID, PositionalIndex}) to our dictionary
                 self.term_doc_id.append((term, (i+1, position_index)))
                 position_index += 1
@@ -70,22 +52,19 @@ class SearchEngine:
                     self.all_tokens_frequencies.update({term: 1})
                 else:
                     self.all_tokens_frequencies.update({term: self.all_tokens_frequencies[term] + 1})
-
         # sort term-DocID dictionary by terms
         self.term_doc_id = self.sort_tuple(self.term_doc_id)
-
         # creating inverted indexes
-        self.inverted_index = self.create_inverted_index(self.term_doc_id)
-
+        self.inverted_index = self.create_inverted_index()
         self.all_tokens_frequencies = dict(sorted(self.all_tokens_frequencies.items(), key=lambda x: x[1]))
-
+        self.inverted_index.pop('')
+        self.all_tokens_frequencies.pop('')
         # removing stopwords
         [self.stopwords.append(list(self.all_tokens_frequencies.keys())[-1 - sw])
          for sw in tqdm(range(self.STOPWORDS_LIMIT), desc='FINDING ALL OF STOPWORDS')]
-        # time.sleep(.1)
-        # print('STOPWORDS: {  ', end='')
-        # [print(sw, end='\t') for sw in self.stopwords]
-        # print('}')
+        self.stopwords_count = []
+        [self.stopwords_count.append(self.all_tokens_frequencies[sw])
+         for sw in self.stopwords]
         [self.inverted_index.pop(sw) for sw in tqdm(self.stopwords, desc='REMOVING SOME STOPWORDS ')]
         end = time.time()
         self.process_time = end - start
@@ -104,10 +83,8 @@ class SearchEngine:
         string_without_substring = text.replace(substring, '').replace("\"", "").strip()
         return substring, string_without_substring.split()
 
-
-
     def get_stemming_dictionary(self):
-        with open('stemming_conversion.txt', 'r', encoding='utf-8') as stems:
+        with open(self.BON_FILE, 'r', encoding='utf-8') as stems:
             dictionary = dict()
             maazi_suffixes = ['م', 'ی', '', 'یم', 'ید', 'ند', 'ن']
             mozare_suffixes = ['م', 'ی', 'د', 'یم', 'ید', 'ند']
@@ -121,7 +98,8 @@ class SearchEngine:
                     dictionary.update({'می' + bon_maazi + s: bon_maazi})  # می‌خواستم می‌خواستی می‌خواست ...
                     dictionary.update({'نمی‌' + bon_maazi + s: bon_maazi})  # نمیخواستم نمیخواستی نمیخواست ...
                     dictionary.update({'نمی' + bon_maazi + s: bon_maazi})  # نمی‌خواستم نمی‌خواستی نمی‌خواست ...
-                    dictionary.update({bon_maazi + 'ه‌ا' + s: bon_maazi})  # خواسته ام خواسته ای ...
+                    dictionary.update({bon_maazi + 'ه‌ا' + s: bon_maazi})  # خواسته‌ام خواسته‌ای ...
+                    dictionary.update({bon_maazi + 'ه‌بود' + s: bon_maazi})  # خواسته‌بودم خواسته‌بودی ...
                 for s in mozare_suffixes:
                     dictionary.update({bon_mozare + s: bon_mozare})  # خواهم خواهی خواهد ...
                     dictionary.update({'ب' + bon_mozare + s: bon_mozare})  # بخواهم بخواهی بخواهد ...
@@ -130,6 +108,7 @@ class SearchEngine:
                     dictionary.update({'نمی' + bon_mozare + s: bon_mozare})  # نمی‌خواهم نمی‌خواهی نمی‌خواهد ...
                     dictionary.update({'می‌' + bon_mozare + s: bon_mozare})  # میخواهم میخواهی میخواهد ...
                     dictionary.update({'می' + bon_mozare + s: bon_mozare})  # می‌خواهم می‌خواهی می‌خواهد ...
+                    dictionary.update({bon_maazi + 'ه‌باش' + s: bon_maazi})  # خواسته‌باشم خواسته‌باشی ...
 
             return dictionary
 
@@ -158,16 +137,15 @@ class SearchEngine:
                     word = self.remove_suffix(word, s)
         return word
 
-    def create_inverted_index(self, term_doc_id):
+    def create_inverted_index(self):
         res = {}
-        for x in tqdm(term_doc_id, desc='CREATING INVERTED INDEX '):
+        for x in tqdm(self.term_doc_id, desc='CREATING INVERTED INDEX '):
             if not x[0] in res.keys():
                 list_ = []
             else:
                 list_ = res[x[0]]
             list_.append(x[1])
             res.update({x[0]: list_})
-
         return res
 
     def stemming_processing(self, term):
@@ -182,51 +160,59 @@ class SearchEngine:
         else:
             return term
 
-    def change_caracters(self, term):
+    def change_characters(self, term):
         for c in self.CHARACTERS_MODIFICATION.keys():
             term = term.replace(c, self.CHARACTERS_MODIFICATION[c])
         return term
 
     def normalize(self, term):
+        """
+        1 - changing characters
+            1.1 - case folding (only for english words)
+            1.2 - change persian numbers into english numbers
+            1.3 - change some persian/arabic characters
+        2 - removing characters
+        3 - removing suffixes
+        4 - changing verbs to stems
+        5 - change mokassar plurals into singular form
+
+        :param term:
+        :return: modified term
+        """
         # removing whitespaces
         term = term.strip()
-
         # case folding for english words
         term = term.lower()
-
-        # removing symbols
-        for symbol in self.SYMBOLS_TO_BE_REMOVED:
-            term = term.replace(symbol, '')
-
-        # change some characters:
-        term = self.change_caracters(term)
-
-        # removing suffixes
-        term = self.removing_suffixes(term)
-
-        # changing verbs to stems ( بن مضارع / بن ماضی )
-        term = self.stemming_processing(term)
-
-        # change mokassar plurals into singular form
-        term = self.mokassar_plurals_processing(term)
-
         # change persian numbers into english numbers
         for fa, en in zip(self.FA_NUMS, self.EN_NUMS):
             term = term.replace(fa, en)
-
+        # change some characters
+        term = self.change_characters(term)
+        # removing symbols
+        for symbol in self.SYMBOLS_TO_BE_REMOVED:
+            term = term.replace(symbol, '')
+        # removing suffixes
+        term = self.removing_suffixes(term)
+        # changing verbs to stems ( بن مضارع / بن ماضی )
+        term = self.stemming_processing(term)
+        # change mokassar plurals into singular form
+        term = self.mokassar_plurals_processing(term)
         return term
 
     def main(self):
-
-        time.sleep(1)
+        time.sleep(.6)
         print('SEARCH ENGINE IS READY ({} seconds)'.format(self.process_time))
-        print(' ├─ {} STOPWORDS'.format(len(self.stopwords)))
+        print(' ├─ {} DOCUMENTS '.format(len(self.content)))
         print(' ├─ {} TOKENS '.format(len(self.term_doc_id)))
+        print(' ├─ {} STOPWORDS'.format(len(self.stopwords)))
+        for sw in range(len(self.stopwords)):
+            if sw != len(self.stopwords)-1:
+                print(' │  ├─ {}/{}:\t({}) \"{}\"'
+                      .format(sw+1, len(self.stopwords), self.stopwords_count[sw], self.stopwords[sw]))
+            else:
+                print(' │  └─ {}/{}:\t({}) \"{}\"'
+                      .format(sw+1, len(self.stopwords), self.stopwords_count[sw], self.stopwords[sw]))
         print(' └─ {} DISTINCT VALUES WITHOUT STOPWORDS'.format(len(self.inverted_index)))
-        # for i in self.all_tokens_frequencies.keys():
-        #     print(i, self.all_tokens_frequencies[i])
-        # for i in self.inverted_index.keys():
-        #     print(i, self.inverted_index[i])
         while True:
             user_queries = input('enter a query \n'
                                  '(\"\\q\": quit the program)\n'
@@ -242,66 +228,60 @@ class SearchEngine:
                 set_of_answers = []
                 missing_words = []
                 substring_results = set()
-
                 # processing substring
                 if len(substring) != 0:
-                    print('processing substring...'.upper())
-                    substring = substring.split()
-                    # print(substring)
-                    candidates = []
-                    for s in substring:
-                        candidate = self.inverted_index[self.normalize(s)]
-                        candidates.append(candidate)
-
-                    # only docs
-                    # print(candidates)
-                    docs = []
-                    for c in candidates:
-                        cc = []
-                        for ccc in c:
-                            cc.append(ccc[0])
-                        docs.append(set(cc))
-                    # print(docs)
-
-                    docs = list(set.intersection(*docs))
-                    # print(docs)
-
-                    if len(docs) == 0:
-                        pass
-                    else:
-                        # first element of substring
-                        for base in candidates[0]:
-                            if base[0] in docs:
-                                flag = True
-                                doc = base[0]
-                                index = base[1]
-                                for i in range(len(candidates)-1):
-                                    if (doc, index + i + 1) not in candidates[i+1]:
-                                        flag = False
-                                        break
-                                if flag:
-                                    substring_results.add(doc)
-
-                    set_of_answers.append(substring_results)
+                    try:
+                        print('processing substring...'.upper())
+                        substring = substring.split()
+                        candidates = []
+                        for s in substring:
+                            candidate = self.inverted_index[self.normalize(s)]
+                            candidates.append(candidate)
+                        docs = []
+                        for c in candidates:
+                            cc = []
+                            for ccc in c:
+                                cc.append(ccc[0])
+                            docs.append(set(cc))
+                        docs = list(set.intersection(*docs))
+                        if len(docs) == 0:
+                            pass
+                        else:
+                            # first element of substring
+                            for base in candidates[0]:
+                                if base[0] in docs:
+                                    flag = True
+                                    doc = base[0]
+                                    index = base[1]
+                                    for i in range(len(candidates)-1):
+                                        if (doc, index + i + 1) not in candidates[i+1]:
+                                            flag = False
+                                            break
+                                    if flag:
+                                        substring_results.add(doc)
+                        set_of_answers.append(substring_results)
+                    except KeyError:
+                        msg = ''
+                        for s in substring:
+                            msg += s + " "
+                        print('SUBSTRING \"{}\" NOT FOUND.'.format(msg))
                 try:
                     for query in string_without_substring:
                         try:
                             if query in self.stopwords:
-                                print('term {} :\tstopword'.format(query).upper())
+                                print('term \"{}\" :\tstopword!'.format(query).upper())
                             else:
                                 result = sorted(list(set(self.inverted_index[self.normalize(query)])))
-                                print('term {} :\t{} result(s)'.format(query.upper(), len(result)))
+                                print('term \"{}\" :\t{} result(s)'.format(query.upper(), len(result)).upper())
                                 result_docs = []
                                 for i in result:
                                     result_docs.append(i[0])
                                 set_of_answers.append(set(result_docs))
-
                         except KeyError:
-                            print('term {} :\tno result for'.upper().format(query))
+                            print('term \"{}\" :\tno result'.upper().format(query).upper())
                             missing_words.append(query)
                 except IndexError:
                     pass
-            # print(set_of_answers)
             print('combining results...'.upper())
             try:
                 result = list(set.intersection(*set_of_answers))
@@ -309,7 +289,6 @@ class SearchEngine:
                 print('NO ANSWERS')
                 print(45 * '- ')
                 continue
-
             if len(result) == 0:
                 print('NO ANSWERS')
                 print(45 * '- ')
@@ -321,15 +300,13 @@ class SearchEngine:
                     print(' ├─ {}'.format(result[r]))
                 else:
                     print(' └─ {}'.format(result[r]))
-
             if len(missing_words) != 0:
                 print('( missing words: '.upper(), end='')
                 for i in range(len(missing_words)):
                     if i == len(missing_words)-1:
                         print(missing_words[i], ')')
                     else:
-                        print(strikedthrough(missing_words[i]), end=', ')
-
+                        print(missing_words[i], end=', ')
             while True:
                 res = input('select on of the results to show the information (-1 to cancel)\n> '.upper())
                 for fa, en in zip(self.FA_NUMS, self.EN_NUMS):
@@ -339,20 +316,15 @@ class SearchEngine:
                     break
                 else:
                     try:
-                        # result_docs = []
-                        # for i in result:
-                        #     result_docs.append(i[0])
                         if int(res) in result:
                             print('DOC-ID : ' + str(self.doc_id[int(res) - 1]))
                             print('LINK   : ' + self.url[int(res) - 1])
                             print('CONTENT: \n' + self.content[int(res) - 1])
-
                         else:
                             print('bad input!'.upper())
                     except Exception as e:
                         print(e)
                         print('bad input!'.upper())
-
             print(45 * '- ')
 
 
