30a00883993f7c694cc21d90508ec366b1e4f1d6
amir78729 <amir78729@gmail.com>
2021-07-12 19:32:40 +0430

    heap class changed. heap sort created

--- Heap.py
+++ Heap.py
@@ -1,126 +1,171 @@
-# Python3 implementation of Max heap
-import sys
+# # Python3 implementation of Max heap
+# import sys
+#
+#
+# class MaxHeap:
+#     def __init__(self, maxsize):
+#         self.maxsize = maxsize
+#         self.size = 0
+#         self.heap = [(0, 0)] * (self.maxsize + 1)
+#         self.heap[0] = (-1, sys.maxsize)
+#         self.front = 1
+#
+#     # Function to return the position of parent for the node currently at pos
+#     def parent(self, pos):
+#         return pos // 2
+#
+#     # Function to return the position of the left child for the node currently  at pos
+#     def leftChild(self, pos):
+#         return 2 * pos
+#
+#     # Function to return the position of the right child for the node currently at pos
+#     def rightChild(self, pos):
+#
+#         return (2 * pos) + 1
+#
+#     # Function that returns true if the passed node is a leaf node
+#     def is_leaf(self, pos):
+#         return (self.size // 2) <= pos <= self.size
+#
+#     # Function to swap two nodes of the heap
+#     def swap(self, fpos, spos):
+#         self.heap[fpos], self.heap[spos] = (self.heap[spos],self.heap[fpos])
+#
+#     # Function to heapify the node at pos
+#     def max_heapify(self, pos):
+#         # If the node is a non-leaf node and smaller
+#         # than any of its child
+#         if not self.is_leaf(pos):
+#             if (self.heap[pos] < self.heap[self.leftChild(pos)] or
+#                     self.heap[pos] < self.heap[self.rightChild(pos)]):
+#
+#                 # Swap with the left child and heapify
+#                 # the left child
+#                 if (self.heap[self.leftChild(pos)] >
+#                         self.heap[self.rightChild(pos)]):
+#                     self.swap(pos, self.leftChild(pos))
+#                     self.max_heapify(self.leftChild(pos))
+#
+#                 # Swap with the right child and heapify
+#                 # the right child
+#                 else:
+#                     self.swap(pos, self.rightChild(pos))
+#                     self.max_heapify(self.rightChild(pos))
+#
+#     # Function to insert a node into the heap
+#     def insert(self, element):
+#         if self.size >= self.maxsize:
+#             return
+#         self.size += 1
+#         self.heap[self.size] = element
+#
+#         current = self.size
+#
+#         # print(self.heap[current])
+#         # print(self.heap[self.parent(current)])
+#         while self.heap[current] > self.heap[self.parent(current)]:
+#
+#             self.swap(current, self.parent(current))
+#             current = self.parent(current)
+#
+#
+#     # Function to print the contents of the heap
+#     def print_heap(self):
+#         try:
+#             self.heap.remove((-1, sys.maxsize))
+#         except ValueError:
+#             pass
+#         for i in range(1, (self.size // 2) + 1):
+#             print(" PARENT : " + str(self.heap[i]) +
+#                   "\n\t LEFT CHILD : " + str(self.heap[2 * i]) +
+#                   "\n\t RIGHT CHILD : " + str(self.heap[2 * i + 1]))
+#
+#     # Function to remove and return the maximum element from the heap
+#     def extract_max(self):
+#         popped = self.heap[self.front]
+#         self.heap[self.front] = self.heap[self.size]
+#         self.size -= 1
+#         self.max_heapify(self.front)
+#         return popped
+#
+#
+# # Driver Code
+# if __name__ == "__main__":
+#     # print('The maxheap is ')
+#
+#     docs = [(1, 0.006021642733869699),
+#             (2, 0.06558860040775293),
+#             (3, 0.007078741382307875),
+#             (4, 0.0030893547785879774),
+#             (5, 0.022719770419340736),
+#             (6, 0.007856149365815733),
+#             (7, 0.006187145066735776),
+#             (8, 0.00640989124608351),
+#             (9, 0.1936971501764423),
+#             (10, 0.008961092746030701)]
+#
+#
+#     maxheap = MaxHeap(len(docs)+2)
+#     for d in docs:
+#         maxheap.insert(d)
+#     maxheap.print_heap()
+#     for i in range(7):
+#
+#         print(maxheap.extract_max())
+#         # print()
+#
+#
+
+# Python program for implementation of heap Sort
+
+# To heapify subtree rooted at index i.
+# n is size of heap
 
 
-class MaxHeap:
-    def __init__(self, maxsize):
-        self.maxsize = maxsize
-        self.size = 0
-        self.heap = [(0, 0)] * (self.maxsize + 1)
-        self.heap[0] = (-1, sys.maxsize)
-        self.front = 1
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
 
-    # Function to return the position of parent for the node currently at pos
-    def parent(self, pos):
-        return pos // 2
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[i][1] < arr[l][1]:
+        largest = l
 
-    # Function to return the position of the left child for the node currently  at pos
-    def leftChild(self, pos):
-        return 2 * pos
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest][1] < arr[r][1]:
+        largest = r
 
-    # Function to return the position of the right child for the node currently at pos
-    def rightChild(self, pos):
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
 
-        return (2 * pos) + 1
-
-    # Function that returns true if the passed node is a leaf node
-    def is_leaf(self, pos):
-        return (self.size // 2) <= pos <= self.size
-
-    # Function to swap two nodes of the heap
-    def swap(self, fpos, spos):
-        self.heap[fpos], self.heap[spos] = (self.heap[spos],self.heap[fpos])
-
-    # Function to heapify the node at pos
-    def max_heapify(self, pos):
-        # If the node is a non-leaf node and smaller
-        # than any of its child
-        if not self.is_leaf(pos):
-            if (self.heap[pos] < self.heap[self.leftChild(pos)] or
-                    self.heap[pos] < self.heap[self.rightChild(pos)]):
-
-                # Swap with the left child and heapify
-                # the left child
-                if (self.heap[self.leftChild(pos)] >
-                        self.heap[self.rightChild(pos)]):
-                    self.swap(pos, self.leftChild(pos))
-                    self.max_heapify(self.leftChild(pos))
-
-                # Swap with the right child and heapify
-                # the right child
-                else:
-                    self.swap(pos, self.rightChild(pos))
-                    self.max_heapify(self.rightChild(pos))
-
-    # Function to insert a node into the heap
-    def insert(self, element):
-        if self.size >= self.maxsize:
-            return
-        self.size += 1
-        self.heap[self.size] = element
-
-        current = self.size
-
-        # print(self.heap[current])
-        # print(self.heap[self.parent(current)])
-        while self.heap[current] > self.heap[self.parent(current)]:
-
-            self.swap(current, self.parent(current))
-            current = self.parent(current)
-
-    # Function to print the contents of the heap
-    def print_heap(self):
-        try:
-            self.heap.remove((-1, sys.maxsize))
-        except ValueError:
-            pass
-        for i in range(1, (self.size // 2) + 1):
-            print(" PARENT : " + str(self.heap[i]) +
-                  "\n\t LEFT CHILD : " + str(self.heap[2 * i]) +
-                  "\n\t RIGHT CHILD : " + str(self.heap[2 * i + 1]))
-
-    # Function to remove and return the maximum element from the heap
-    def extract_max(self):
-        popped = self.heap[self.front]
-        self.heap[self.front] = self.heap[self.size]
-        self.size -= 1
-        self.max_heapify(self.front)
-        return popped
+        # Heapify the root.
+        heapify(arr, n, largest)
 
 
-# Driver Code
-if __name__ == "__main__":
-    # print('The maxheap is ')
+# The main function to sort an array of given size
+def heapSort(arr):
+    n = len(arr)
 
-    docs = [(1, 0.006021642733869699),
-            (2, 0.06558860040775293),
-            (3, 0.007078741382307875),
-            (4, 0.0030893547785879774),
-            (5, 0.022719770419340736),
-            (6, 0.007856149365815733),
-            (7, 0.006187145066735776),
-            (8, 0.00640989124608351),
-            (9, 0.1936971501764423),
-            (10, 0.008961092746030701)]
+    # Build a maxheap.
+    # Since last parent will be at ((n//2)-1) we can start at that location.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
 
-
-    maxheap = MaxHeap(len(docs)+2)
-    for d in docs:
-        maxheap.insert(d)
-    # maxheap.insert(5)
-    # maxheap.insert(3)
-    # maxheap.insert(17)
-    # maxheap.insert(10)
-    # maxheap.insert(84)
-    # maxheap.insert(19)
-    # maxheap.insert(6)
-    # maxheap.insert(22)
-    # maxheap.insert(9)
-
-    # maxheap.Print()
-    for i in range(8):
-        # maxheap.print_heap()
-        print(maxheap.extract_max())
-        # print()
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+    res = arr[::-1]
+    return res[:4]
 
 
+# Driver code to test above
+arr = [(3, 0.026366538469134372), (4, 0.03502408665140206), (5, 0.038308875408798584), (6, 0.011197565580587961), (7, 0.014278479396708945), (8, 0.08238434069802868), (9, 0.10622119680238401)]
+
+arr = heapSort(arr)
+n = len(arr)
+print("Sorted array is")
+for i in range(n):
+    print(arr[i])
--- Main.py
+++ Main.py
@@ -4,92 +4,140 @@ import time
 import re
 import math
 import numpy as np
+import sys
+
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
+
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[i][1] < arr[l][1]:
+        largest = l
+
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest][1] < arr[r][1]:
+        largest = r
+
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, n, largest)
 
 
-class MaxHeap:
-    def __init__(self, maxsize):
-        self.maxsize = maxsize
-        self.size = 0
-        self.heap = [(0, 0)] * (self.maxsize + 1)
-        self.heap[0] = (-1, sys.maxsize)
-        self.front = 1
+# The main function to sort an array of given size
+def heapSort(arr, k):
+    n = len(arr)
 
-    # Function to return the position of parent for the node currently at pos
-    def parent(self, pos):
-        return pos // 2
+    # Build a maxheap.
+    # Since last parent will be at ((n//2)-1) we can start at that location.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
 
-    # Function to return the position of the left child for the node currently  at pos
-    def leftChild(self, pos):
-        return 2 * pos
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+    res = arr[::-1]
+    return res[:k]
 
-    # Function to return the position of the right child for the node currently at pos
-    def rightChild(self, pos):
 
-        return (2 * pos) + 1
-
-    # Function that returns true if the passed node is a leaf node
-    def is_leaf(self, pos):
-        return (self.size // 2) <= pos <= self.size
-
-    # Function to swap two nodes of the heap
-    def swap(self, fpos, spos):
-        self.heap[fpos], self.heap[spos] = (self.heap[spos],self.heap[fpos])
-
-    # Function to heapify the node at pos
-    def max_heapify(self, pos):
-        # If the node is a non-leaf node and smaller
-        # than any of its child
-        if not self.is_leaf(pos):
-            if (self.heap[pos] < self.heap[self.leftChild(pos)] or
-                    self.heap[pos] < self.heap[self.rightChild(pos)]):
-
-                # Swap with the left child and heapify
-                # the left child
-                if (self.heap[self.leftChild(pos)] >
-                        self.heap[self.rightChild(pos)]):
-                    self.swap(pos, self.leftChild(pos))
-                    self.max_heapify(self.leftChild(pos))
-
-                # Swap with the right child and heapify
-                # the right child
-                else:
-                    self.swap(pos, self.rightChild(pos))
-                    self.max_heapify(self.rightChild(pos))
-
-    # Function to insert a node into the heap
-    def insert(self, element):
-        if self.size >= self.maxsize:
-            return
-        self.size += 1
-        self.heap[self.size] = element
-
-        current = self.size
-
-        # print(self.heap[current])
-        # print(self.heap[self.parent(current)])
-        while self.heap[current] > self.heap[self.parent(current)]:
-
-            self.swap(current, self.parent(current))
-            current = self.parent(current)
-
-    # Function to print the contents of the heap
-    def print_heap(self):
-        try:
-            self.heap.remove((-1, sys.maxsize))
-        except ValueError:
-            pass
-        for i in range(1, (self.size // 2) + 1):
-            print(" PARENT : " + str(self.heap[i]) +
-                  "\n\t LEFT CHILD : " + str(self.heap[2 * i]) +
-                  "\n\t RIGHT CHILD : " + str(self.heap[2 * i + 1]))
-
-    # Function to remove and return the maximum element from the heap
-    def extract_max(self):
-        popped = self.heap[self.front]
-        self.heap[self.front] = self.heap[self.size]
-        self.size -= 1
-        self.max_heapify(self.front)
-        return popped
+# class MaxHeap:
+#     def __init__(self, maxsize):
+#         self.maxsize = maxsize
+#         self.size = 0
+#         self.heap = [(0, 0)] * (self.maxsize + 1)
+#         self.heap[0] = (-1, sys.maxsize)
+#         self.front = 1
+#
+#     # Function to return the position of parent for the node currently at pos
+#     def parent(self, pos):
+#         return pos // 2
+#
+#     # Function to return the position of the left child for the node currently  at pos
+#     def leftChild(self, pos):
+#         return 2 * pos
+#
+#     # Function to return the position of the right child for the node currently at pos
+#     def rightChild(self, pos):
+#
+#         return (2 * pos) + 1
+#
+#     # Function that returns true if the passed node is a leaf node
+#     def is_leaf(self, pos):
+#         return (self.size // 2) <= pos <= self.size
+#
+#     # Function to swap two nodes of the heap
+#     def swap(self, fpos, spos):
+#         self.heap[fpos], self.heap[spos] = (self.heap[spos],self.heap[fpos])
+#
+#     # Function to heapify the node at pos
+#     def max_heapify(self, pos):
+#         # If the node is a non-leaf node and smaller
+#         # than any of its child
+#         if not self.is_leaf(pos):
+#             if (self.heap[pos] < self.heap[self.leftChild(pos)] or
+#                     self.heap[pos] < self.heap[self.rightChild(pos)]):
+#
+#                 # Swap with the left child and heapify
+#                 # the left child
+#                 if (self.heap[self.leftChild(pos)] >
+#                         self.heap[self.rightChild(pos)]):
+#                     self.swap(pos, self.leftChild(pos))
+#                     self.max_heapify(self.leftChild(pos))
+#
+#                 # Swap with the right child and heapify
+#                 # the right child
+#                 else:
+#                     self.swap(pos, self.rightChild(pos))
+#                     self.max_heapify(self.rightChild(pos))
+#
+#     # Function to insert a node into the heap
+#     def insert(self, element):
+#         if self.size >= self.maxsize:
+#             return
+#         self.size += 1
+#         self.heap[self.size] = element
+#
+#         current = self.size
+#
+#         # print(self.heap[current])
+#         # print(self.heap[self.parent(current)])
+#         while self.heap[current] > self.heap[self.parent(current)]:
+#
+#             self.swap(current, self.parent(current))
+#             current = self.parent(current)
+#
+#     # Function to print the contents of the heap
+#     def print_heap(self):
+#         try:
+#             self.heap.remove((-1, sys.maxsize))
+#         except ValueError:
+#             pass
+#         for i in range(1, (self.size // 2) + 1):
+#             try:
+#                 print(" PARENT : " + str(self.heap[i]))
+#             except:
+#                 pass
+#             try:
+#                 print("\t LEFT CHILD : " + str(self.heap[2 * i]))
+#             except:
+#                 pass
+#             try:
+#                 print("\t RIGHT CHILD : " + str(self.heap[2 * i + 1]))
+#             except:
+#                 pass
+#
+#     # Function to remove and return the maximum element from the heap
+#     def extract_max(self):
+#         popped = self.heap[self.front]
+#         self.heap[self.front] = self.heap[self.size]
+#         self.size -= 1
+#         self.max_heapify(self.front)
+#         return popped
 
 
 class SearchEngine:
@@ -106,6 +154,7 @@ class SearchEngine:
         self.SYMBOLS_TO_BE_REMOVED = '_ + = & ؟ ^ % $ . : ، \" \' | \\ / * ) ( ! - ؛'.split()
         self.SUFFIXES_TO_BE_REMOVED = ['ترین', 'تر', 'ات', 'ها', 'ی', '‌شان', 'ان']
         self.STOPWORDS_LIMIT = 20
+        self.TOP_RANK_NUMBERS = 4
         self.CHARACTERS_MODIFICATION = {
             'آ': 'ا',
             'ي': 'ی',
@@ -217,10 +266,6 @@ class SearchEngine:
         #     print(i)
         #     print('*'*20)
 
-        print(sorted(self.document_frequency.keys()))
-        print(sorted(self.all_tokens_frequencies.keys()))
-
-
         # removing stopwords
         [self.stopwords.append(list(self.all_tokens_frequencies.keys())[-1 - sw])
          for sw in tqdm(range(self.STOPWORDS_LIMIT), desc='FINDING ALL OF STOPWORDS')]
@@ -278,8 +323,6 @@ class SearchEngine:
             return 0
         return tf * idf
 
-
-
     def query_vector_space(self, query):
         vector = []
         for T in tqdm(self.document_frequency.keys(), 'CREATING QUERY VEC.SPACE'):
@@ -301,13 +344,37 @@ class SearchEngine:
     def query_similarity(self, user_query_vector_space):
         i = 1
         lst = []
+        test = []
+        # heap = MaxHeap(len(self.doc_id) + 2)
         for doc in tqdm(self.vector_space.keys(), 'CALCULATING Q SIMILARITY'):
             similarity = self.cosine_similarity(self.vector_space[doc], user_query_vector_space)
             if similarity != 0.0 and not math.isnan(similarity):
                 lst.append((i, similarity))
+                # insert item to heap
+                # heap.insert((i, similarity))
             i += 1
+        print(test)
+        # print(lst)
+        # heap.heap.remove((-1, sys.maxsize))
+        # while (0, 0) in heap.heap:
+        #     heap.heap.remove((0, 0))
+        # heap.heap.append((0, 0))
+        # heap.heap.append((0, 0))
+        # print(heap.heap)
+        # heap.print_heap()
+
+        # # returning top K docs by their score
+        # for i in range(self.TOP_RANK_NUMBERS):
+        #     # maxheap.print_heap()
+        #     m = heap.extract_max()
+        #     print(m)
+        #     lst.append(m)
+        top_k = heapSort(lst, self.TOP_RANK_NUMBERS)
+        # print(top_k)
+
         # return sorted(lst, key=lambda x: x[1])[::-1]
-        return lst
+        # print(lst)
+        return top_k
 
     # Function to sort the list by second item of tuple
     def sort_tuple(self, tup):
@@ -466,11 +533,9 @@ class SearchEngine:
                                  '(\"\\q\": quit the program)\n'
                                  '(you can use \"[a substring]\" in search)\n> '.upper())
             substring, string_without_substring = self.exstract_substring(user_queries)
+
             user_query_vector_space = self.query_vector_space(user_queries)
-            print(user_query_vector_space.shape)
-            print(self.vector_space[1].shape)
-            print('-'*20)
-            print(self.query_similarity(user_query_vector_space))
+            top_k_documents = self.query_similarity(user_query_vector_space)
 
             start_time = time.time()
             if user_queries == '':
